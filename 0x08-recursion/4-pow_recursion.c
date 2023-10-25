2213768e0c3e711695f7e93cd89ba7586c889e24
revolk <finalrevolk@gmail.com>
2023-10-25 02:00:53 -0700

    recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -5,20 +5,17 @@
   * @y: parameter 2
   * Return: value of a given number passed to the function
   */
-
-
 int _pow_recursion(int x, int y)
 {
 
-if (y < 0)
-        return (-1);
+	if (y < 0)
+        	return (-1);
 
-else if (y == 0)
-        return (1);
+	else if (y == 0)
+        	return (1);
 
-else
-        return (x * _pow_recursion(x, (y - 1)));
-
+	else
+        	return (x * _pow_recursion(x, (y - 1)));
 return (0);
 }
 
